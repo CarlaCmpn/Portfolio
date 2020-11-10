53d6e9a4b5fb2ab75ee6422961083402da92e10f
CarlaCmpn <compan.carla@gmail.com>
2020-11-10 20:38:37 +0100

    add selected page menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,22 +16,28 @@
 		<div class="navbar-open" v-bind:class="[isActive ? 'transition-open' : 'transition-close']">
 			<div class="list">
 				<div class="list-link" v-on:click="isActive = false">
+					<div v-if="selectedWork" class="circle"></div>
 					<router-link to="/">
 						<p>Works</p>
 						<div class="underligne"></div>
 					</router-link>
+					<div v-if="selectedWork" class="circle"></div>
 				</div>
 				<div class="list-link" v-on:click="isActive = false">
+					<div v-if="selectedAbout" class="circle"></div>
 					<router-link to="/about">
 						<p>About</p>
 						<div class="underligne"></div>
 					</router-link>
+					<div v-if="selectedAbout" class="circle"></div>
 				</div>
 				<div class="list-link" v-on:click="isActive = false">
+					<div v-if="selectedContact" class="circle"></div>
 					<router-link to="/contact">
 						<p>Contact</p>
 						<div class="underligne"></div>
 					</router-link>
+					<div v-if="selectedContact" class="circle"></div>
 				</div>
 			</div> 
 		</div>
@@ -46,16 +52,41 @@ export default {
 	components:{
 		
 	},
-	data() {
+	data(){
 		return {
 			isActive: false,
+			selectedWork: false,
+			selectedAbout: false,
+			selectedContact: false,
 		}
 	},
-	methods: {
-		hamburger: function() {
+	methods:{
+		hamburger: function(){
 			this.isActive = !this.isActive
+		},
+	},
+	updated(){
+		let currentUrl = window.location.pathname;
+		console.log(currentUrl);
+
+		if(currentUrl === "/"){
+			this.selectedWork = true
+		}else{
+			this.selectedWork = false
 		}
-  	}
+
+		if(currentUrl === "/about"){
+			this.selectedAbout = true
+		}else{
+			this.selectedAbout = false
+		}
+
+		if(currentUrl === "/contact"){
+			this.selectedContact = true
+		}else{
+			this.selectedContact = false
+		}
+	}
 }
 </script>
 
